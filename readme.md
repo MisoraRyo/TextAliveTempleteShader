bd28c2deec21ea5e86c6f47eb9893018d41220ac
MisoraRyo <69433648+MisoraRyo@users.noreply.github.com>
2023-04-08 19:52:47 +0900

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,22 @@
+# TextAlive Templete Ver 0.1
+
+私的に使っているTextAlive APP開発用のベースプロジェクトファイル_Ver0.1
+
+正常に実行するにはTextAliveのトークンが必要です。
+
+<img src="./static/image.png" width="100%"/>
+
+# Getting Started
+Download and install Node.js on your computer (https://nodejs.org/en/download/).
+
+Then, open VSCODE, drag the project folder to it. Open VSCODE terminal and install dependencies (you need to do this only in the first time)
+```
+npm install
+```
+
+Run this command in your terminal to open a local server at localhost:8080
+```
+npm run dev
+```
+
+# _
---
+++ src/fragment.glsl
@@ -0,0 +1,77 @@
+//https://misora.main.jp/shader_01/
+
+#define R_LUMINANCE 0.298912
+#define G_LUMINANCE 0.586611
+#define B_LUMINANCE 0.114478
+const vec3 monochromeScale = vec3(R_LUMINANCE, G_LUMINANCE, B_LUMINANCE);
+
+uniform sampler2D udisplayment;//ﾃｦ窶凪｡ﾃ･ﾂｭ窶氾ｧ窶敖ｻﾃ･ﾆ陳湘｣ﾂ�ｮﾃ｣ﾆ停｡ﾃ｣ﾆ陳ｼﾃ｣窶堋ｿ
+
+uniform vec2 resolution;
+uniform float uTime;
+
+const float Pi = 3.14159;
+uniform vec2 mouse;
+
+const int   complexity      = 30;    // More points of color.
+const float mouse_factor    = 56.0;  // Makes it more/less jumpy.
+const float mouse_offset    = 0.0;   // Drives complexity in the amount of curls/cuves.  Zero is a single whirlpool.
+const float fluid_speed     = 2.0;  // Drives speed, higher number will make it slower.
+const float color_intensity = 0.7;
+
+const float fMosaicScale = 30.0;
+
+varying vec2 vUv;
+
+void main()
+{
+  //gl_FragCoord_
+
+  vec4 disp = texture2D(udisplayment, vUv);
+  float alpha = disp.r * 0.2 + disp.g * 0.7 + disp.b * 0.1;
+  
+  //if (alpha > .9 ) {
+    
+  vec2 p = ( 2.0 * gl_FragCoord.xy - resolution ) / max(resolution.x,resolution.y);
+
+  for(int i=1;i<complexity;i++)
+  {
+    vec2 newp = p + uTime*0.001;
+    newp.x+=0.6/float(i)*cos(float(i)*p.y+uTime/fluid_speed+0.3*float(i)) + 0.5; // + mouse.y/mouse_factor+mouse_offset;
+    newp.y+=0.6/float(i)*sin(float(i)*p.x+uTime/fluid_speed+0.3*float(i+10)) - 0.5; // - mouse.x/mouse_factor+mouse_offset;
+    p = newp;
+  }
+  
+  vec3 col = vec3(
+    color_intensity*sin(1.0*p.x)+color_intensity,
+    color_intensity*sin(3.0*p.y)+color_intensity,
+    //1.0,
+    //1.0,
+    0.0
+    //color_intensity*sin(p.x+p.y)+color_intensity
+    //color_intensity*sin(p.x+p.y)+color_intensity
+  );
+
+  float grayColor = dot(col.rgb, monochromeScale);
+  vec3 Finalcolor = vec3(grayColor) * vec3(1.0,1.0,1.0);
+  /*
+  vec2 vUv2 = vUv;
+  vUv2.x = floor(vUv2.x * resolution.x / fMosaicScale) / (resolution.x / fMosaicScale) + (fMosaicScale / 2.0) / resolution.x;
+  vUv2.y = floor(vUv2.y * resolution.y / fMosaicScale) / (resolution.y / fMosaicScale) + (fMosaicScale / 2.0) / resolution.y;
+  */
+
+  //vec4 color = texture2D(grayColor, vUv2);
+  //float alpha = Finalcolor.r * 0.2 + Finalcolor.g * 0.7 + Finalcolor.b * 0.1;
+
+  //gl_FragColor = vec4(Finalcolor, 1.0);
+
+  gl_FragColor = vec4(vec3(1.0,1.0,1.0) - col, 1.0);
+
+  /*}else{
+
+    discard;
+
+  }*/
+
+  
+}
